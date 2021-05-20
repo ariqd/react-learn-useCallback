e24f1b381916c0057f43916fb77ad6b7be477691
Ariq Daffa <ariqdaffaathallah@gmail.com>
2021-05-20 13:23:12 +0700

    after-usecallback

--- src/App.js
+++ src/App.js
@@ -1,13 +1,13 @@
-import { useState } from "react";
+import { useState, useCallback } from "react";
 import List from "./List";
 
 export default function App() {
   const [number, setNumber] = useState(1);
   const [dark, setDark] = useState(false);
 
-  const getItems = () => {
+  const getItems = useCallback(() => {
     return [number, number + 1, number + 2];
-  };
+  }, [number]);
 
   const theme = {
     backgroundColor: dark ? "#333" : "#FFF",
